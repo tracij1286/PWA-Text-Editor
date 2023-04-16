86dfd86284a74ae2e5dfc5c86caad2bb60039892
tracij1286 <tracij50@gmail.com>
2023-04-16 15:06:51 -0400

    added logic to method in database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,51 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+	console.log("PUT to the database");
+
+	// Create a connection to the database and version we want to use.
+	const jateDb = await openDB("jate", 1);
+
+	// Create a new transaction and specify the database and data privileges.
+	const tx = jateDb.transaction("jate", "readwrite");
+
+	// Open up the desired object store.
+	const store = tx.objectStore("jate");
+
+	// Use the .put() method to update data in the database.
+	// The text editor consists of one field of information that is repeatedly retrieved and updated.
+	const request = store.put({ id: 1, value: content });
+
+	// Get confirmation of the request.
+	const result = await request;
+	console.log("data saved to the database", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+	console.log("GET from the database");
+
+	// Create a connection to the database and version we want to use.
+	const jateDb = await openDB("jate", 1);
+
+	// Create a new transaction and specify the database and data privileges.
+	const tx = jateDb.transaction("jate", "readonly");
+
+	// Open up the desired object store.
+	const store = tx.objectStore("jate");
+
+	// Use the .get() method to get the one text editor entry from the database.
+	const request = store.get(1);
+
+	// Get confirmation of the request.
+	const result = await request;
+	// If there is a text editor entry, return it.
+	result
+		? console.log(" data retrieved from the database", result.value)
+		: console.log(" data not found in the database");
+	return result?.value;
+};
+
 
 initdb();
